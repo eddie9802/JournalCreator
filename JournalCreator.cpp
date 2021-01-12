ac485f5e9e8848285ee30ab4c755fe72684e3feb
eddie9802 <ehamilton9810@gmail.com>
2021-01-12 10:47:18 +0000

    Program can now create .jr files

--- JournalCreator.cpp
+++ JournalCreator.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
 
 using namespace std;
+
+// Takes in the users questions for the journal and returns them in a vector
+vector<string> getJournalQuestions() {
+    vector<string> allQuestions;
+
+    // At least one question is required
+    string question;
+
+    // Reads user questions until no question is entered
+    do {
+        getline(cin, question);
+        // Checks if a question mark is at the end
+        if (question[question.length() - 1] == '?') {
+            allQuestions.push_back(question);
+            cout << "\nQuestion successfully added.  Please enter another question or enter nothing to complete the journal creation process\n";
+        } else if (question.length() > 0) {
+            cout << "Questions must end in a question mark" << endl;
+        } else if (allQuestions.size() == 0) {
+            cout << "Atleast one question must be given" << endl;
+        }
+    } while (allQuestions.size() == 0 || question.length() > 0);
+    return allQuestions;
+}
 
 
 // Creates a .jr file
@@ -18,21 +42,19 @@ void createJournalFile() {
     cout << "\nPlease enter the questions you would like to ask the user on addition of journal entries";
     cout << "\nTo enter a question write out your question and press enter.  If you don't want to enter any more questions enter nothing\n";
 
-    // Takes in the users questions for the journal
-    vector<string> allQuestions;
-    string question;
-    getline(cin, question);
-    allQuestions.push_back(question);
-    while (question.length() > 0) {
-        cout << "\nPlease enter another queston\n";
-        getline(cin, question);
-        allQuestions.push_back(question);
-    }
+    vector<string> allQuestions = getJournalQuestions();
 
+    // Writes questions to the file given by journalName and path filepath.  Creates a .jr file
+    filepath.append(journalName);
+    filepath.append(".jr");
+    ofstream outfile(filepath);
+
+    outfile << "Journal Questions" << endl;
+    // Writes all of the questions to the start of the file
     for (int i = 0; i < allQuestions.size(); i++) {
-        cout << allQuestions[i] << "\n";
+        outfile << allQuestions[i];
     }
-
+    outfile.close();
 }
 
 void addToJournal() {
@@ -46,9 +68,8 @@ void startJournalCreator() {
     getline(cin, answer);
 
     // Checks users answer to previous y/n question
-    bool createJournal = (answer == "y") ? true : false;
 
-    if (createJournal) {
+    if (answer == "y") {
         createJournalFile();
     } else {
         addToJournal();
