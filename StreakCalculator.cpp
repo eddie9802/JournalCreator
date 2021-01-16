5226c7620ec0bef222f62599af2383b6f9d007f7
eddie9802 <ehamilton9810@gmail.com>
2021-01-16 11:32:24 +0000

    Changed all references to JournalCreator to StreakCreator

--- StreakCalculator.cpp
+++ StreakCalculator.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 // Takes in the users questions for the journal and returns them in a vector
-vector<string> getJournalQuestions() {
+vector<string> getStreakQuestions() {
     vector<string> allQuestions;
 
     // At least one question is required
@@ -18,7 +18,7 @@ vector<string> getJournalQuestions() {
         // Checks if a question mark is at the end
         if (question[question.length() - 1] == '?') {
             allQuestions.push_back(question);
-            cout << "\nQuestion successfully added.  Please enter another question or enter nothing to complete the journal creation process\n";
+            cout << "\nQuestion successfully added.  Please enter another question or enter nothing to complete the streak file creation process\n";
         } else if (question.length() > 0) {
             cout << "Questions must end in a question mark" << endl;
         } else if (allQuestions.size() == 0) {
@@ -30,26 +30,26 @@ vector<string> getJournalQuestions() {
 
 
 // Creates a .jr file
-void createJournalFile() {
-    cout << "What would you like to call your journal?\n";
-    string journalName;
-    getline(cin, journalName);
+void createStreakFile() {
+    cout << "What would you like to call your streak file?\n";
+    string streakFileName;
+    getline(cin, streakFileName);
 
-    cout << "\nWhere would you like your journal to be located?  Please specify the filepath as DIRNAME/ ...etc\n";
+    cout << "\nWhere would you like your streak file to be located?  Please specify the filepath as DIRNAME/ ...etc\n";
     string filepath;
     getline(cin, filepath);
 
-    cout << endl << "Please enter the questions you would like to ask the user on addition of journal entries";
+    cout << endl << "Please enter the questions you would like to ask the user on opening of streak file";
     cout << endl << "To enter a question write out your question and press enter.  If you don't want to enter any more questions enter nothing" << endl;
 
-    vector<string> allQuestions = getJournalQuestions();
+    vector<string> allQuestions = getStreakQuestions();
 
-    // Writes questions to the file given by journalName and path filepath.  Creates a .jr file
-    filepath.append(journalName);
-    filepath.append(".jr");
+    // Writes questions to the file given by streakFileName and path filepath.  Creates a .stk file
+    filepath.append(streakFileName);
+    filepath.append(".stk");
     ofstream outfile(filepath);
 
-    outfile << "Journal Questions" << endl;
+    outfile << "Streak file questions" << endl;
     // Writes all of the questions to the start of the file
     for (int i = 0; i < allQuestions.size(); i++) {
         outfile << allQuestions[i];
@@ -57,28 +57,28 @@ void createJournalFile() {
     outfile.close();
 }
 
-void addToJournal() {
+void addToStreakFile() {
 
 }
 
-void startJournalCreator() {
+void startStreakCalculator() {
     // Prints welcome message, asks user if they would like to create a journal and gets their response
-    cout << "Welcome to JournalCreator.  Would you like to create a journal? y/n\n";
+    cout << "Welcome to StreakCalculator.  Would you like to create a streak file? y/n\n";
     string answer;
     getline(cin, answer);
 
     // Checks users answer to previous y/n question
 
     if (answer == "y") {
-        createJournalFile();
+        createStreakFile();
     } else {
-        addToJournal();
+        addToStreakFile();
     }
 
 
 }
 
 int main(int argc, char* argv[]) {
-    startJournalCreator();
+    startStreakCalculator();
     return 0;
 }
